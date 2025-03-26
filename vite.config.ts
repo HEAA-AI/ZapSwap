bd35b3bfd07f41a58a013ac4093e53a168a471c1
Ritikchhipa5 <ritikchhipa5@gmail.com>
2025-03-26 15:03:54 +0530

    fix the buffer issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import path from "path";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
